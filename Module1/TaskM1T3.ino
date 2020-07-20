f159be870e0a10016ded91845369a952421120c7
elpeacey <63824453+elpeacey@users.noreply.github.com>
2020-07-20 14:45:00 +1000

    Add files via upload

--- Module1/TaskM1T3.ino
+++ Module1/TaskM1T3.ino
@@ -1,17 +1,27 @@
-int led = 13;         // set LED pin
+#include "pins_arduino.h"
+
+// set LED pins
+int redLED = 10;
+int greenLED = 12;
+
 int motion = 2;       // set motion sensor pin 
-int tempPin = 0;	  // set temp sensor pin	
+int motion2 = 3;	  // set second motion pin
+
 
 //Global variables
-float temp;
-int val = LOW;                    // read pin status
+int red = LOW;
+int green = LOW;
+
  
 void setup() {
-  pinMode(led, OUTPUT);      // declare LED as output
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
   pinMode(motion, INPUT);     // declare sensor as input
+  pinMode(motion2, INPUT);
   
   // set interrupt
-  attachInterrupt(digitalPinToInterrupt(motion), toggle, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(motion), toggleRed, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(motion2), toggleGreen, CHANGE);
   
   Serial.begin(9600);
   
@@ -19,24 +29,32 @@ void setup() {
 }
  
 void loop(){
-  temp = analogRead(tempPin);
-  temp = (temp  * 0.48828125);
-  Serial.print("Temperature in celsius: ");
-  Serial.print(temp);
-  Serial.println();
-  
   delay(1000);
 }
 
-void toggle() {
-  val = !val;
-  if (val == HIGH) {          // check if the input is HIGH
-    digitalWrite(led, HIGH);  // turn LED ON
-    Serial.println("LED on");
+void toggleRed() {
+  red = !red;
+  if (red == HIGH) {          // check if the input is HIGH
+    digitalWrite(redLED, HIGH);  // turn LED ON
+    Serial.println("LED red");
     Serial.println("Motion detected");
   }
   else {
-    digitalWrite(led, LOW); // turn LED OFF
+    digitalWrite(redLED, LOW); // turn LED OFF
+    Serial.println("LED off");
+    Serial.println("Motion ended");
+  }
+}
+
+void toggleGreen() {
+  red = !red;
+  if (red == HIGH) {          // check if the input is HIGH
+    digitalWrite(greenLED, HIGH);  // turn LED ON
+    Serial.println("LED green");
+    Serial.println("Motion detected");
+  }
+  else {
+    digitalWrite(greenLED, LOW); // turn LED OFF
     Serial.println("LED off");
     Serial.println("Motion ended");
   }
