2944caecfde6b690b9a473433cbf7f0ef909872c
elpeacey <63824453+elpeacey@users.noreply.github.com>
2020-08-21 12:30:25 +1000

    Add files via upload

---
+++ Module1/TaskM1T5/bin1/arduino-uno/atmega_common/startup.d
@@ -0,0 +1,149 @@
+/home/mpiuser/RIOT/examples/M1T5/bin/arduino-uno/atmega_common/startup.o: \
+ /home/mpiuser/RIOT/cpu/atmega_common/startup.c \
+ /home/mpiuser/RIOT/examples/M1T5/bin/arduino-uno/riotbuild/riotbuild.h \
+ /opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /opt/arduino/hardware/tools/avr/avr/include/stdint.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/interrupt.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/io.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/sfr_defs.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/avr_libc_extra/include/inttypes.h \
+ /opt/arduino/hardware/tools/avr/avr/include/inttypes.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/iom328p.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/portpins.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/common.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/version.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/fuse.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/lock.h \
+ /home/mpiuser/RIOT/boards/arduino-uno/include/board.h \
+ /home/mpiuser/RIOT/boards/common/arduino-atmega/include/board_common.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/include/cpu.h \
+ /opt/arduino/hardware/tools/avr/avr/include/stdio.h \
+ /opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/include/cpu_conf.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/include/atmega_regs_common.h \
+ /opt/arduino/hardware/tools/avr/avr/include/avr/power.h \
+ /home/mpiuser/RIOT/core/include/sched.h \
+ /home/mpiuser/RIOT/core/include/kernel_defines.h \
+ /home/mpiuser/RIOT/core/include/bitarithm.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/include/cpu_conf.h \
+ /home/mpiuser/RIOT/core/include/kernel_types.h \
+ /opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include-fixed/limits.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/avr_libc_extra/include/sys/types.h \
+ /home/mpiuser/RIOT/core/include/native_sched.h \
+ /home/mpiuser/RIOT/core/include/clist.h \
+ /home/mpiuser/RIOT/core/include/list.h \
+ /home/mpiuser/RIOT/core/include/thread.h \
+ /home/mpiuser/RIOT/core/include/cib.h \
+ /home/mpiuser/RIOT/core/include/assert.h \
+ /home/mpiuser/RIOT/core/include/panic.h \
+ /home/mpiuser/RIOT/core/include/msg.h \
+ /opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stdbool.h \
+ /home/mpiuser/RIOT/core/include/sched.h \
+ /home/mpiuser/RIOT/core/include/irq.h \
+ /home/mpiuser/RIOT/boards/common/arduino-atmega/include/arduino_pinmap.h \
+ /home/mpiuser/RIOT/drivers/include/periph/gpio.h \
+ /home/mpiuser/RIOT/cpu/atmega328p/include/periph_cpu.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/include/periph_cpu_common.h \
+ /home/mpiuser/RIOT/cpu/atmega_common/include/cpu.h \
+ /home/mpiuser/RIOT/boards/common/arduino-atmega/include/periph_conf.h \
+ /home/mpiuser/RIOT/boards/common/atmega/include/periph_conf_atmega_common.h \
+ /home/mpiuser/RIOT/drivers/include/periph/adc.h
+
+/home/mpiuser/RIOT/examples/M1T5/bin/arduino-uno/riotbuild/riotbuild.h:
+
+/opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/stdint.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/interrupt.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/io.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/sfr_defs.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/avr_libc_extra/include/inttypes.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/inttypes.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/iom328p.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/portpins.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/common.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/version.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/fuse.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/lock.h:
+
+/home/mpiuser/RIOT/boards/arduino-uno/include/board.h:
+
+/home/mpiuser/RIOT/boards/common/arduino-atmega/include/board_common.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/include/cpu.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/stdio.h:
+
+/opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/include/cpu_conf.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/include/atmega_regs_common.h:
+
+/opt/arduino/hardware/tools/avr/avr/include/avr/power.h:
+
+/home/mpiuser/RIOT/core/include/sched.h:
+
+/home/mpiuser/RIOT/core/include/kernel_defines.h:
+
+/home/mpiuser/RIOT/core/include/bitarithm.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/include/cpu_conf.h:
+
+/home/mpiuser/RIOT/core/include/kernel_types.h:
+
+/opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include-fixed/limits.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/avr_libc_extra/include/sys/types.h:
+
+/home/mpiuser/RIOT/core/include/native_sched.h:
+
+/home/mpiuser/RIOT/core/include/clist.h:
+
+/home/mpiuser/RIOT/core/include/list.h:
+
+/home/mpiuser/RIOT/core/include/thread.h:
+
+/home/mpiuser/RIOT/core/include/cib.h:
+
+/home/mpiuser/RIOT/core/include/assert.h:
+
+/home/mpiuser/RIOT/core/include/panic.h:
+
+/home/mpiuser/RIOT/core/include/msg.h:
+
+/opt/arduino/hardware/tools/avr/lib/gcc/avr/5.4.0/include/stdbool.h:
+
+/home/mpiuser/RIOT/core/include/sched.h:
+
+/home/mpiuser/RIOT/core/include/irq.h:
+
+/home/mpiuser/RIOT/boards/common/arduino-atmega/include/arduino_pinmap.h:
+
+/home/mpiuser/RIOT/drivers/include/periph/gpio.h:
+
+/home/mpiuser/RIOT/cpu/atmega328p/include/periph_cpu.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/include/periph_cpu_common.h:
+
+/home/mpiuser/RIOT/cpu/atmega_common/include/cpu.h:
+
+/home/mpiuser/RIOT/boards/common/arduino-atmega/include/periph_conf.h:
+
+/home/mpiuser/RIOT/boards/common/atmega/include/periph_conf_atmega_common.h:
+
+/home/mpiuser/RIOT/drivers/include/periph/adc.h:
